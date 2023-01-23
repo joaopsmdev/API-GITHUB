c96d68217dd1c914d1a936b89e2c27bba2cfd3da
joaopsmdev <joaopedrosouzamarinho0@gmail.com>
2023-01-23 16:09:28 -0300

    deploying two

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { langColors } from './config';
 
 const api = axios.create({
-  baseURL: 'https://api.github.com/',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 export const getUser = async (login) => api.get(`/users/${login}`);
 
